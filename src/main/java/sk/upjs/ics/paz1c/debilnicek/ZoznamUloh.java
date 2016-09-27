793f07c598098893d4bec15ac32ddf771c0e8401
kriza <richard.stana@upjs.sk>
2016-09-27 08:37:50 +0200

    zaciatok projektu

---
+++ src/main/java/sk/upjs/ics/paz1c/debilnicek/ZoznamUloh.java
@@ -0,0 +1,41 @@
+package sk.upjs.ics.paz1c.debilnicek;
+
+import java.util.*;
+
+class ZoznamUloh {
+
+    private List<Uloha> ulohy = new ArrayList<Uloha>();
+
+    public ZoznamUloh() {
+        Uloha uloha1 = new Uloha();
+        uloha1.setPopis("Programovat test");
+        uloha1.setTermin(new Date(2016 - 1999, Calendar.SEPTEMBER, 28));
+
+        ulohy.add(uloha1);
+
+        Uloha uloha2 = new Uloha();
+        uloha2.setPopis("Naprog pouz roz");
+        uloha2.setTermin(new Date(2016 - 1999, Calendar.OCTOBER, 5));
+
+        ulohy.add(uloha2);
+    }
+
+    List<Uloha> dajUlohy() {
+        return ulohy;
+    }
+
+    void pridaj(Uloha uloha) {
+        ulohy.add(uloha);
+    }
+
+    List<Uloha> dajDoTerminu(Date date) {
+        List<Uloha> ulohyDoTerminu = new ArrayList<>();
+        for (Uloha uloha : ulohy) {
+            if (uloha.getTermin().before(date)) {
+                ulohyDoTerminu.add(uloha);
+            }
+        }
+        return ulohyDoTerminu;
+    }
+
+}
